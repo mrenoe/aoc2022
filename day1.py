6edc4dee410a0a92c85a47b149d7d07136e7b6f2
Mike Renoe <mrenoe@gmail.com>
2022-12-01 21:01:41 -0500

    day1

---
+++ day1.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+def get_input():
+  input = defaultdict(list)
+  with open("day1.txt", "r") as tf:
+    line = tf.readline()
+    i = 0
+    while line:
+      line = line.rstrip('\n')
+      if len(line) > 0:
+        input[i].append(int(line))
+      else:
+        i += 1
+      line = tf.readline()
+  return input
+
+def part1():
+  input = get_input()
+  max = 0
+  for i in input:
+    s = sum(input[i])
+    if s > max:
+      max = s
+  print(max)
+
+
+def part2():
+  input = get_input()
+  sums = []
+  for i in input:
+    sums.append(sum(input[i]))
+
+  sums.sort()
+  print(sum(sums[-3:]))
+
+
+
+
+
+if __name__ == '__main__':
+  #part1()
+  part2()
